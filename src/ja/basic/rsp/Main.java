9bd8dfb2f8adbfbbf60f356078c50562126df849
AkiStory <sonsungjeon@gmail.com>
2025-10-23 14:16:40 +0900

    java_practice_lesson_25.10.21_homework

--- src/ja/basic/rsp/Main.java
+++ src/ja/basic/rsp/Main.java
@@ -7,7 +7,9 @@ package ja.basic.rsp;
 public class Main {
 	static void main(String[] args) {
 		System.out.println("가위바위보를 질 때까지 합니다.");
+
 		Winner.whoWin();
+
 		System.out.println("\n가위바위보를 종료합니다.");
 	}
 }
